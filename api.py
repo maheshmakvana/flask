b8c35d3d8e7581a46676ee4098ea32d2b998fb2d
Mahesh Makwana <51027062+maheshmakwana527@users.noreply.github.com>
2020-12-29 12:52:36 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,148 @@
+import flask
+from flask import request, jsonify
+import time
+import MetaTrader5 as mt5
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
+
+
+@app.route('/', methods=['GET'])
+def home():
+    # display data on the MetaTrader 5 package
+    print("MetaTrader5 package author: ", mt5.__author__)
+    print("MetaTrader5 package version: ", mt5.__version__)
+
+    # establish connection to the MetaTrader 5 terminal
+    if not mt5.initialize():
+        print("initialize() failed, error code =", mt5.last_error())
+        quit()
+
+    # prepare the buy request structure
+    symbol = "USDCHF"
+    symbol_info = mt5.symbol_info(symbol)
+    if symbol_info is None:
+        print(symbol, "not found, can not call order_check()")
+        mt5.shutdown()
+        quit()
+
+    # if the symbol is unavailable in MarketWatch, add it
+    if not symbol_info.visible:
+        print(symbol, "is not visible, trying to switch on")
+        if not mt5.symbol_select(symbol, True):
+            print("symbol_select({}}) failed, exit", symbol)
+            mt5.shutdown()
+            quit()
+
+    lot = 0.01
+    point = mt5.symbol_info(symbol).point
+    print('----------------------Point-------------------------', str(point))
+    price = mt5.symbol_info_tick(symbol).ask
+    print('------------------------Price-----------------------', price)
+
+    deviation = 20
+    request = {
+        "action": mt5.TRADE_ACTION_DEAL,
+        "symbol": symbol,
+        "volume": lot,
+        "type": mt5.ORDER_TYPE_BUY,
+        "price": price,
+        "sl": price - 3000 * point,
+        "tp": price + 3000 * point,
+        "deviation": deviation,
+        "magic": 234000,
+        "comment": "python script open",
+        "type_time": mt5.ORDER_TIME_GTC,
+        "type_filling": mt5.ORDER_FILLING_RETURN,
+    }
+
+    # send a trading request
+    result = mt5.order_send(request)
+    # check the execution result
+    print("1. order_send(): by {} {} lots at {} with deviation={} points".format(symbol, lot, price, deviation));
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+        print("2. order_send failed, retcode={}".format(result.retcode))
+        # request the result as a dictionary and display it element by element
+        result_dict = result._asdict()
+        for field in result_dict.keys():
+            print("   {}={}".format(field, result_dict[field]))
+            # if this is a trading request structure, display it element by element as well
+            if field == "request":
+                traderequest_dict = result_dict[field]._asdict()
+                for tradereq_filed in traderequest_dict:
+                    print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+        print("shutdown() and quit")
+        mt5.shutdown()
+        quit()
+
+    print("2. order_send done, ", result)
+    print("   opened position with POSITION_TICKET={}".format(result.order))
+    print("   sleep 2 seconds before closing position #{}".format(result.order))
+    time.sleep(2)
+    # create a close request
+    position_id = result.order
+    price = mt5.symbol_info_tick(symbol).bid
+    deviation = 20
+    request = {
+        "action": mt5.TRADE_ACTION_DEAL,
+        "symbol": symbol,
+        "volume": lot,
+        "type": mt5.ORDER_TYPE_SELL,
+        "position": position_id,
+        "price": price,
+        "deviation": deviation,
+        "magic": 234000,
+        "comment": "python script close",
+        "type_time": mt5.ORDER_TIME_GTC,
+        "type_filling": mt5.ORDER_FILLING_RETURN,
+    }
+    # send a trading request
+    # result = mt5.order_send(request)
+    # check the execution result
+    print(
+        "3. close position #{}: sell {} {} lots at {} with deviation={} points".format(position_id, symbol, lot, price,
+                                                                                       deviation));
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+        print("4. order_send failed, retcode={}".format(result.retcode))
+        print("   result", result)
+    else:
+        print("4. position #{} closed, {}".format(position_id, result))
+        # request the result as a dictionary and display it element by element
+        result_dict = result._asdict()
+        for field in result_dict.keys():
+            print("   {}={}".format(field, result_dict[field]))
+            # if this is a trading request structure, display it element by element as well
+            if field == "request":
+                traderequest_dict = result_dict[field]._asdict()
+                for tradereq_filed in traderequest_dict:
+                    print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+
+    # shut down connection to the MetaTrader 5 terminal
+    mt5.shutdown()
+    return result
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify(books)
+
+app.run()
